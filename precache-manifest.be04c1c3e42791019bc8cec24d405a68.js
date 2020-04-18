9ac7dd3c82670a261c01ac6a8892fc68415db9c0
Ahmad Asroni <ahmadasroni38@gmail.com>
2020-04-18 14:40:06 +0800

    Updates

---
+++ precache-manifest.be04c1c3e42791019bc8cec24d405a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dc49b3e9c239cd00869b571fe02e48",
+    "url": "/wpr-corona/index.html"
+  },
+  {
+    "revision": "9581a254978ae39e35a5",
+    "url": "/wpr-corona/static/css/main.304015aa.chunk.css"
+  },
+  {
+    "revision": "e36cfe1c43d95c58182e",
+    "url": "/wpr-corona/static/js/2.ded01dc8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/wpr-corona/static/js/2.ded01dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9581a254978ae39e35a5",
+    "url": "/wpr-corona/static/js/main.48cf0adb.chunk.js"
+  },
+  {
+    "revision": "bee22644985954b4f888",
+    "url": "/wpr-corona/static/js/runtime-main.949617f5.js"
+  }
+]);
